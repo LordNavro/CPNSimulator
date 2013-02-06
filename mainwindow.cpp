986f85c3d24be784274a6cb4bbdd0b57420f86e3
Ondřej Navrátil <lord-navro@LordNavro-NB.(none)>
2013-02-06 18:37:02 +0100

    icons preview

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,10 +4,50 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    QLabel *l = new QLabel("test", this);
+    createActions();
+    createToolBars();
+    createMenuBars();
 }
 
 MainWindow::~MainWindow()
 {
-    
+}
+
+void MainWindow::createActions()
+{
+    actionNew = new QAction(QIcon(":icons/icons/new.ico"), tr("New net"), this);
+    actionSave = new QAction(QIcon(":icons/icons/save.ico"), tr("Save net"), this);
+    actionLoad = new QAction(QIcon(":icons/icons/load.ico"), tr("Load net"), this);
+
+    actionPlace = new QAction(QIcon(":icons/icons/place.ico"), tr("New place"), this);
+    actionTransition = new QAction(QIcon(":icons/icons/transition.ico"), tr("New transition"), this);
+    actionArc = new QAction(QIcon(":icons/icons/arc.ico"), tr("New arc"), this);
+    actionDelete = new QAction(QIcon(":icons/icons/delete.ico"), tr("Delete object"), this);
+
+    actionAbout = new QAction(QIcon(":icons/icons/about.ico"), tr("About program"), this);
+}
+
+void MainWindow::createToolBars()
+{
+    toolBarFile = addToolBar(tr("File options"));
+    toolBarSimulation = addToolBar(tr("Simulation options"));
+    toolBarTools = addToolBar(tr("Tool options"));
+
+    toolBarFile->addAction(actionNew);
+    toolBarFile->addAction(actionSave);
+    toolBarFile->addAction(actionLoad);
+
+    toolBarTools->addAction(actionPlace);
+    toolBarTools->addAction(actionTransition);
+    toolBarTools->addAction(actionArc);
+    toolBarTools->addAction(actionDelete);
+
+}
+
+void MainWindow::createMenuBars()
+{
+    menuFile = menuBar()->addMenu(QIcon(":icons/icons/file.ico"), tr("File"));
+    menuFile->addAction(actionNew);
+    menuFile->addAction(actionSave);
+    menuFile->addAction(actionLoad);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,11 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <QtGui>
+
+
+//icons: http://www.iconfinder.com/search/?q=iconset%3Afatcow+control
+
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +14,31 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private:
+    QAction *actionNew;
+    QAction *actionSave;
+    QAction *actionLoad;
+
+    QAction *actionPlace;
+    QAction *actionTransition;
+    QAction *actionArc;
+    QAction *actionDelete;
+
+    QAction *actionAbout;
+
+    QToolBar *toolBarFile;
+    QToolBar *toolBarTools;
+    QToolBar *toolBarSimulation;
+
+    QMenu *menuFile;
+    QMenu *menuTool;
+    QMenu *menuSimulation;
+    QMenu *menuAbout;
+
+    void createActions(void);
+    void createToolBars(void);
+    void createMenuBars(void);
 };
 
 #endif // MAINWINDOW_H
